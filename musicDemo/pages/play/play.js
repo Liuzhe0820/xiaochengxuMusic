4ceff12288e46e0d30a2c8fd18d2b3685b82d2d7
liuzhe0820 <liu55zhe#163.com>
2019-04-09 17:51:05 +0800

    1

--- musicDemo/pages/play/play.js
+++ musicDemo/pages/play/play.js
@@ -20,26 +20,30 @@ Page({
     wx.request({
       url: url.musicDetail + '?ids=' + id,
       success: (res) => {
-        // console.log(res.data.songs[0]);
+        console.log(res.data.songs[0]);
         this.setData({
           song: res.data.songs[0]
-        })
+        });
+        let { song } = app.globalData;
+        if (!song) {
+          song = app.globalData.song = wx.createInnerAudioContext();
+          song = app.globalData.song = wx.getBackgroundAudioManager();
+          song.title = this.data.song.name;
+        };
+        song.src = `http://music.163.com/song/media/outer/url?id=${id}.mp3`;
+
+        song.play();
+        song.onPlay(res => {
+          console.log('开始');
+        });
+        song.onTimeUpdate(function () {
+          console.log(1)
+        });
       }
     });
-    let { song } = app.globalData;
-    if (!song) {
-      song = app.globalData.song = wx.createInnerAudioContext();
-    };
-    // song.src = `http://music.163.com/song/media/outer/url?id=${id}.mp3`;
-    // song.src ='http://static.missevan.com/MP3/201705/08/c8ab947c6a85092e5ffadcf02bbe348b184435.mp3';
-    song.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46';
-    song.play();
-    song.onPlay(res => {
-      console.log('开始');
-    });
-    song.onTimeUpdate(function(){
-      console.log(1)
-    });
+
+
+
     // wx.request({
     //   url: url.musicPlay +'?id='+id,
     //   success:(res)=>{
